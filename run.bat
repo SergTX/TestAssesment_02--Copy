baa7e3ab63fe118174b3ef7d87c2c428fd44beb9
SergTX <SergeyShvaiuk@gmail.com>
2022-06-12 15:22:56 -0500

    Commit 001

---
+++ run.bat
@@ -0,0 +1,10 @@
+
+pytest -v -s -m 'smoke or regression' --html='..//Reports//report.html' --browser chrome
+
+rem pytest -v -s -m 'regression' --html='..//Reports//report.html' --browser chrome
+
+rem pytest -v -s -m 'smoke or regression' --html='..//Reports//report.html' --browser Firefox
+
+rem pytest -v -s -m "smoke or  regression" --alluredir="C:\Users\serge\OneDrive\Desktop\TestAssesment_01\Reports\allureReports" --browser chrome
+
+rem pytest -v -s -m "smoke or  regression" --alluredir="C:\Users\serge\OneDrive\Desktop\TestAssesment_01\Reports\allureReports" --browser Firefox
